3b3524be2b23179dad75b7da84ead42bef2f4cec
Vani satish <65238042+vanisatish@users.noreply.github.com>
2020-07-09 22:33:38 -0500

    Update UpdateResource.py

--- GET_Request/UpdateResource.py
+++ GET_Request/UpdateResource.py
@@ -6,7 +6,7 @@ import jsonpath
 url = 'https://reqres.in/api/users/2'
 
 # read data from json file
-file = open('C:/Users/satis/APIAutomation/updateResource.json', 'r')
+file = open('C:/Users/APIAutomation/updateResource.json', 'r')
 json_input = file.read()
 request_json = json.loads(json_input)
 
