efe15e2cbb5af73caf97c6579314a967304fdc6d
Vani satish <65238042+vanisatish@users.noreply.github.com>
2020-07-09 22:14:43 -0500

    Update test_end_to_end.py

--- TestCases/test_end_to_end.py
+++ TestCases/test_end_to_end.py
@@ -3,17 +3,6 @@ import jsonpath
 import requests
 
 
-def test_Add_new_data():
-    # adding a student
-    # app_url = "http://thetestingworldapi.com/api/studentsDetails"
-    # file = open('C:/Users/satis/AppData/Local/Programs/Python/Python37-32/APIAutomation/GET_Request/Requestjson.json', 'r')
-    # request_json = json.loads(file.read())
-    # response = requests.post(app_url, request_json)
-    # print(response.text)
-    # id = jsonpath.jsonpath(response.json(), 'id')
-    # print(id[0])
-
-
     # adding technical details
     tech_api_url = "http://thetestingworldapi.com/api/technicalskills"
     file = open('C:/Users/satis/AppData/Local/Programs/Python/Python37-32/APIAutomation/GET_Request/postjson.json', 'r')
